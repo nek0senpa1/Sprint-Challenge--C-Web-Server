59396a1c4d74b13613d18978d873f9ca9c9d1cb4
amberm <nekosenpai@gmail.com>
2019-07-12 13:46:40 -0600

    adding  final function info

--- src/client.c
+++ src/client.c
@@ -49,13 +49,13 @@ urlinfo_t *parse_url(char *url)
     *firstSlash = '/0';
 
     // 4. Use strchr to find the first colon in the URL.
-    char *col= strchr(hostname, ':')
+    char *col= strchr(hostname, ':');
 
     // 5. Set the port pointer to 1 character after the spot returned by strchr.
     port = col+1;
 
     // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-    col = '/0'
+    col = '/0';
   
 
   ///////////////////
@@ -115,18 +115,34 @@ int main(int argc, char *argv[])
   int sockfd, numbytes;  
   char buf[BUFSIZE];
 
+
+  struct urlinfo_t urlinfo;
+
   if (argc != 2) {
     fprintf(stderr,"usage: client HOSTNAME:PORT/PATH\n");
     exit(1);
   }
 
-  /*
-    1. Parse the input URL
-    2. Initialize a socket by calling the `get_socket` function from lib.c
-    3. Call `send_request` to construct the request and send it
-    4. Call `recv` in a loop until there is no more data to receive from the server. Print the received response to stdout.
-    5. Clean up any allocated memory and open file descriptors.
-  */
+  
+    // 1. Parse the input URL
+    urlinfo = parse_url(argv[1]);
+
+    // 2. Initialize a socket by calling the `get_socket` function from lib.c
+    sockfd = get_socket(urlinfo->hostname, urlinfo->port);
+
+    // 3. Call `send_request` to construct the request and send it
+    send_request(sockfd, urlinfo->hostname, urlinfo->port, urlinfo->path);
+
+    // 4. Call `recv` in a loop until there is no more data to receive from the server. Print the received response to stdout.
+    while ((numbytes = recv(sockfd, buf, BUFSIZE - 1, 0)) > 0) {
+
+      printf("%s", buf);
+      }
+
+    // 5. Clean up any allocated memory and open file descriptors.
+
+    free(urlinfo);
+  
 
   ///////////////////
   // IMPLEMENT ME! //
